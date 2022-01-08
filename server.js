9536a833b98db98bcf29b4e954df89ceba4bb88d
samantha frangi <sefrangi13@gmail.com>
2022-01-08 10:57:41 -0500

    wip: set up basic router and test server success

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 //    DEPENDENCIES
 
 // get .env variables
-require 
+require('dotenv').config();
+
+// pull PORT form .env, give default value of 3001
+const { PORT = 3001 } = process.env
+// const PORT  = process.env || 3001 ** alternate way to write **
+
+// import express from 'express'
+const express = require('express')
+
+// create application object
+const app = express()
+
+//      ROUTES
+
+// create test route -- this is great for future practice.
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+
+// listeners 
+app.listen(PORT, () => console.log(`${PORT}`))
