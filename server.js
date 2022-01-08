4c613d7c8898ed1e5eac6b606601c8ed5b924956
samantha frangi <sefrangi13@gmail.com>
2022-01-08 11:53:40 -0500

    wip: adds index and post routes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//    DEPENDENCIES
+//                                              DEPENDENCIES
 
 // get .env variables
-require('dotenv').config();
+require('dotenv').config()
 
 // pull PORT form .env, give default value of 3001
-const { PORT = 3001 } = process.env
+const { PORT = 3001, DATABASE_URL } = process.env
 // const PORT  = process.env || 3001 ** alternate way to write **
 
 // import express from 'express'
@@ -13,13 +13,61 @@ const express = require('express')
 // create application object
 const app = express()
 
-//      ROUTES
+// import mongoose
+const mongoose = require('mongoose')
+
+// import middleware... req => middleware => route => res
+const cors = require('cors')
+const morgan = require('morgan') 
+
+//                                      --  DATABASE CONNECTION --
+
+// establish connection
+mongoose.connect(DATABASE_URL)
+
+// connection Events
+mongoose.connection
+  .on("open", () => console.log("open"))
+  .on("close", () => console.log("close"))
+  .on("error", (error) => console.log(error))
+  //                                             --  MODELS --
+  const PeopleSchema = new mongoose.Schema ( {
+    name: String,
+    image: String,
+    title: String,
+  })
+
+  const People = mongoose.model("People", PeopleSchema)
+
+  //                                            -- MIDDLEWARE --
+  app.use(cors()) // to prevent cors errors, opens access to all data
+  app.use(morgan("dev")) //logging
+  app.use(express.json()) // parse json bodies 
+
+  //                                             --  ROUTES --
 
 // create test route -- this is great for future practice.
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+// people index route
+// async/await - there is a promise running "under the hood" and the function waits until that promise is fulfilled before running???
+app.get('/people', async(req, res) =>{
+  try{
+    res.json(await People.find({})) // waits until the app finds all the people in the array before sending the response
+  } catch(error) {
+    res.status(400).json(error)
+  }
+})
+// people create route
+app.post("/people", async (req, res) => {
+  try {
+    res.json(await People.create(req.body))
+  } catch(error) {
+    res.status(400).json(error)
+  }
+})
 
-// listeners 
+//                                               LISTENER
 app.listen(PORT, () => console.log(`${PORT}`))
